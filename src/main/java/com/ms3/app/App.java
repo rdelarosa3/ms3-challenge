c28122a044fcd1167f153df83b8b83a5f6b28277
Robert De la Rosa <rdelarosa39@gmail.com>
2021-02-19 02:49:17 -0600

    Feat: java terminal inteface complete

--- src/main/java/com/ms3/app/App.java
+++ src/main/java/com/ms3/app/App.java
@@ -3,24 +3,79 @@ package com.ms3.app;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
-import java.util.Scanner;
 
 public class App {
+    private static File file; // gets file from input
+    private static String fname; // file name to use for db,table,directory
+    private static final String[] menuItems = {"Import CSV", "Exit"}; // menu items for cli
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);  // scanner to get user input
-        System.out.println("Enter your CSV file path: ");
-        String input = in.nextLine(); // gets users input in a string
-        File file = new File(input); // gets file from input
-
-        if (!file.getName().toLowerCase().endsWith(".csv")){    // verifies file is CSV extension
-             System.err.println("Invalid File Type CSV Required");
-             return;
+        boolean runProgram = true;
+        while (runProgram){
+            printMenu();
+            runProgram = getMenuInput();
         }
+        System.out.println("Program Terminated");
+    }
 
-        String fname = FilenameUtils.removeExtension(file.getName().toLowerCase()); // get filename w/out extension
-        createDirectory(fname); // creates directory from filename
+    /** creates a directory based on the file name to store db, logfile and csv **/
+    public static void createDirectory(String fname){
+        File directory = new File(fname);
+        if(!directory.exists()) {
+            if(directory.mkdir()){
+                System.out.println("Creating Directory");
+                parseDataToDB();
+            }else{
+                System.err.println("Unable to create Directory");
+            }
+        }else{
+            System.err.println("Directory Already Exists. OverWrite?");
+            if(Input.yesNo()) {
+                parseDataToDB();
+            }
+        }
+    }
 
+    /** interface for to acknowledge user **/
+    public static void printMenu(){
+        System.out.println(("What would you like to do?"));
+        for (int i = 0; i < menuItems.length; i++) {
+            System.out.println((i + 1) + ". " + menuItems[i]);
+        }
+    }
+
+    /** gets the users input from menu items **/
+    public static boolean getMenuInput(){
+        System.out.println("Enter an option (1 through "+menuItems.length+"):");
+        // validate user input
+        switch (Input.getInt(1,menuItems.length)){
+            case 1:
+                getCSVFile();
+                break;
+            case 2:
+                return false;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    /** gets user csv from user input **/
+    public static void getCSVFile(){
+        System.out.println("Enter your CSV file path: ");
+        String input = Input.getString(); // scanner to get user input
+        File tempFile = new File(input); // gets file from input
+        if (!tempFile.getName().toLowerCase().endsWith(".csv")){    // verifies file is CSV extension
+            System.err.println("Invalid File Type CSV Required");
+        }else{
+            file = tempFile;
+            fname = FilenameUtils.removeExtension(file.getName().toLowerCase()); // get filename w/out extension
+            createDirectory(fname); // creates directory from filename
+        }
+    }
+
+    /** driving method  **/
+    public static void parseDataToDB(){
         // initialize CSV Parser to read the csv and validate data
         CsvParser csvParser = new CsvParser(fname);
         csvParser.readCSV(file);
@@ -35,15 +90,5 @@ public class App {
         /* writes files into directory */
         csvParser.writeFailedCsv();
         csvParser.writeLogFile();
-    }
-
-    /** creates a directory based on the file name to store db, logfile and csv **/
-    public static void createDirectory(String fname){
-        File directory = new File(fname);
-        if(!directory.exists()) {
-            System.out.println(directory.mkdir() ? "Creating Directory" : "Unable to create Directory");
-        }else{
-            System.out.println("Directory Already Exists");
-        }
     }
 }
--- src/main/java/com/ms3/app/CsvSQLiteDao.java
+++ src/main/java/com/ms3/app/CsvSQLiteDao.java
@@ -20,8 +20,8 @@ public class CsvSQLiteDao {
             connection = DriverManager.getConnection(jdbcUrl);  // create connection
             if (connection != null) {
                 DatabaseMetaData meta = connection.getMetaData();
-                System.out.println("Using Driver Name : " + meta.getDriverName());
-                System.out.println("Db has been created");
+                System.out.println(ANSI_BLUE+"Using Driver Name : " + meta.getDriverName() +ANSI_RESET);
+                System.out.println(ANSI_BLUE+"Db has been created"+ANSI_RESET);
             }
         } catch (SQLException e) {
             System.err.println("Something went wrong");
@@ -32,7 +32,7 @@ public class CsvSQLiteDao {
     /** creates table in db based on the file name and names columns from csv header **/
     public void createTable(List<String> headers) {
         StringBuilder sql = new StringBuilder("CREATE TABLE IF NOT EXISTS " + fname + " ("); // sql table stmnt
-        System.out.println("Creating table for DB");
+        System.out.println(ANSI_BLUE+"Creating table for DB"+ANSI_RESET);
         int index = 0;  // index value to use in switch
         insertString = ""; // create string to use in insert sql based on column names
         // creates table columns and data types from csv header list
@@ -66,7 +66,7 @@ public class CsvSQLiteDao {
         try {
             Statement stmt = connection.createStatement();
             stmt.execute(sql.toString());
-            System.out.println("Table Created Successfully");
+            System.out.println(ANSI_BLUE+"Table Created Successfully"+ANSI_RESET);
         } catch (SQLException e) {
             System.err.println("Failed To Create Table");
             System.err.println(e.getMessage());
@@ -75,11 +75,17 @@ public class CsvSQLiteDao {
 
     /** inserts values from the Obj model created from csv rows with valid data **/
     public void insert(List<CsvModel> dbData) {
-        System.out.println("Inserting data into table");
+        System.out.println(ANSI_BLUE+"Inserting data into table"+ANSI_RESET);
+        int count = 3;
         try {
             // Loops through the Obj Models and sets each value by datatype into table.
             // using prepared stmnt to prevent sql injection
             for (CsvModel c : dbData) {
+                if (count % 100 == 0) {
+                    System.out.println();
+                } else {
+                    System.out.print(".");
+                }
                 String sql = "INSERT INTO ms3interview (" + insertString + ") VALUES(?,?,?,?,?,?,?,?,?,?)";
                 PreparedStatement pstmt = connection.prepareStatement(sql);
                 pstmt.setString(1, c.getA());
@@ -93,12 +99,18 @@ public class CsvSQLiteDao {
                 pstmt.setBoolean(9, c.isI());
                 pstmt.setString(10, c.getJ());
                 pstmt.executeUpdate();
+                count++;
             }
-            System.out.println("Data insert complete");
+            System.out.println();
+            System.out.println(ANSI_BLUE+"Data insert complete"+ANSI_RESET);
         } catch (SQLException e) {
             System.err.println("Failed to insert data into table");
             System.err.println(e.getMessage());
 
         }
     }
+
+    /** colors for interface **/
+    public static final String ANSI_BLUE = "\033[0;34m";    // BLUE
+    public static final String ANSI_RESET = "\033[0m";  // Text Reset
 }
