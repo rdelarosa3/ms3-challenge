4f69450b20df2d5a63b1d7590479d3d3f231a9bd
Robert De la Rosa <rdelarosa39@gmail.com>
2021-02-18 18:14:05 -0600

    Removed sout from app

--- src/main/java/com/ms3/app/App.java
+++ src/main/java/com/ms3/app/App.java
@@ -3,8 +3,5 @@ package com.ms3.app;
 public class App {
     public static void main(String[] args) {
         CsvParser csvParser = new CsvParser("ms3Interview.csv");
-        System.out.println("Received: "+csvParser.getReceived());
-        System.out.println("Success: "+csvParser.getSuccess());
-        System.out.println("Failed: "+csvParser.getFailed());
     }
 }
